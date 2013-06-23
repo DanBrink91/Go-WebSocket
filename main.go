aed760c67710e60644e2f66daa22ae7916840f34
DanBrink91 <DanBrink91@gmail.com>
2013-06-22 22:38:44 -0400

    Added template parsing, removed server.go

--- main.go
+++ main.go
@@ -1,22 +1,26 @@
 package main
 
 import(
-    "fmt"
     "code.google.com/p/go.net/websocket"
     "flag"
     "log"
     "net/http"
+    "html/template"
 )
+
 var addr = flag.String("addr", ":8080", "http service address")
+var homeTempl = template.Must(template.ParseFiles("test.html"))
 
 func homeHandler(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+    homeTempl.Execute(w, r.Host)
 }
 func main(){
     flag.Parse()
     go h.run()
+
     http.HandleFunc("/", homeHandler)
     http.Handle("/ws", websocket.Handler(wsHandler))
+
     if err := http.ListenAndServe(*addr, nil); err != nil {
             log.Fatal("ListenAndServe:", err)
     }
